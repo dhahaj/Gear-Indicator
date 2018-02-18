41c19aafd21e0cd727a53580edaaad1c039e62c9
Daniel Hahaj <dhahaj@fedora.attlocal.net>
2018-02-17 23:43:53 -0600

    Changed neutral to be an active low input.

--- GearIndicator.ino
+++ GearIndicator.ino
@@ -5,11 +5,11 @@
 //------------------------------------------
 
 #include <avr/io.h>
-#include <util/delay.h>
+//#include <util/delay.h>
 #include <avr/eeprom.h>
 
 // Set to 1 to enable active low neutral input
-#define NEUTRAL_ACTIVE_LOW 0
+//#define NEUTRAL_ACTIVE_HIGH 0
 
 // PIN/PORT DEFINITIONS
 #define PORT          PORTB
@@ -29,32 +29,32 @@
 
 // Specific Configurations
 #define TOP_GEAR      9
-#define SHOW_DIGITS(n) showDigit((n)); _delay_ms(575)
+#define SHOW_DIGITS(n) showDigit((n)); delay(575)
 
 /*
- * Display a digit on the 7-segment display.
- */
+   Display a digit on the 7-segment display.
+*/
 void showDigit(uint8_t Digit) {
-    uint8_t i;
+  uint8_t i;
 
-    PORT |= (1 << RESET_BIT);       // RESET pin high
-    PORT &= ~(1 << RESET_BIT);      // RESET pin low
+  PORT |= (1 << RESET_BIT);       // RESET pin high
+  PORT &= ~(1 << RESET_BIT);      // RESET pin low
 
-    for (uint8_t i = 0; i < Digit; i++) {
-        PORT |= (1 << CLOCK_BIT);   // CLOCK pin high
-        PORT &= ~(1 << CLOCK_BIT);  // CLOCK pin low
-    }
+  for (uint8_t i = 0; i < Digit; i++) {
+    PORT |= (1 << CLOCK_BIT);   // CLOCK pin high
+    PORT &= ~(1 << CLOCK_BIT);  // CLOCK pin low
+  }
 }
 
 /*
- * Simple debouncing for the gear inputs
- */
+   Simple debouncing for the gear inputs
+*/
 int debouce(uint8_t pin) {
-    if (bit_is_clear(PIN, pin)) {
-        _delay_ms(DEBOUNCE_TIME);
-        if (bit_is_set(PIN, pin)) return true;
-    }
-    return false;
+  if (bit_is_clear(PIN, pin)) {
+    delay(DEBOUNCE_TIME);
+    if (bit_is_set(PIN, pin)) return true;
+  }
+  return false;
 }
 
 volatile uint8_t gear;
@@ -64,28 +64,29 @@ volatile uint8_t gear;
 */
 void setup(void) {
 
-#if (NEUTRAL_ACTIVE_LOW)
-    PORT = ( _BV(UP_BIT) | _BV(DOWN_BIT) | _BV(NEUTRAL_BIT) ); // Enable all pullups
-#else
-    PORT = ( _BV(UP_BIT) | _BV(DOWN_BIT) ); // UP and DOWN pin enable pull-up
-#endif
+  //#if (NEUTRAL_ACTIVE_HIGH)
+  PORT = ( _BV(UP_BIT) | _BV(DOWN_BIT) | _BV(NEUTRAL_BIT) ); // Enable all pullups
+  //#else
+  //  PORT = ( _BV(UP_BIT) | _BV(DOWN_BIT) ); // UP and DOWN pin enable pull-up
+  //#endif
 
-    DDPORT |= _BV(RESET_DDR);      // RESET pin as output
-    DDPORT |= _BV(CLOCK_DDR);      // CLOCK pin as output
+  DDPORT |= _BV(RESET_DDR);      // RESET pin as output
+  DDPORT |= _BV(CLOCK_DDR);      // CLOCK pin as output
 
-    PORT &= ~(_BV(RESET_BIT));  // RESET pin low
+  PORT &= ~(_BV(RESET_BIT));  // RESET pin low
 
-    // Self test effect
-    showDigit(0);
-    _delay_ms(350);
-    for (uint8_t i = 1; i < 10; i++)
-        SHOW_DIGITS(i);
+  // Self test effect
+  showDigit(0);
+  delay(350);
+  for (uint8_t i = 1; i < 10; i++) {
+    showDigit(i);
+    delay(500);
+  }
+  showDigit(0);
+  delay(500);
 
-    showDigit(0);
-    _delay_ms(500);
-
-    gear = eeprom_read_byte((uint8_t*)0);  // Read initial value from EEPROM
-    showDigit(gear);                       // Show initial value
+  gear = eeprom_read_byte((uint8_t*)0);  // Read initial value from EEPROM
+  showDigit(gear);                       // Show initial value
 }
 
 /*
@@ -93,49 +94,49 @@ void setup(void) {
 */
 void loop(void) {
 
-    /***  UP SHIFT  ***/
-    if (debouce(UP_BIT))
-    {
-        // Some logic here :-)
-        (gear > 0 && gear < TOP_GEAR) ? gear++ : \
-        (gear == 0) ? gear = 2 : NULL;
-        // if ( (gear > 0) && (gear < TOP_GEAR) ) gear++;
-        // if (gear == 0) gear = 2;
+  /***  UP SHIFT  ***/
+  if (debouce(UP_BIT))
+  {
+    // Some logic here :-)
+    //        (gear > 0 && gear < TOP_GEAR) ? gear++ : \
+    //        (gear == 0) ? gear = 2 : NULL;
+    if (gear == 0) gear = 2;
+    else if ( (gear > 0) && (gear < TOP_GEAR) ) gear++;
 
-        uint8_t bit;
-#if (NEUTRAL_ACTIVE_LOW)
-        bit = !bit_is_clear(PIN, NEUTRAL_BIT);
+    uint8_t bit;
+#if (NEUTRAL_ACTIVE_HIGH)
+    bit = !bit_is_clear(PIN, NEUTRAL_BIT);
 #else
-        bit = bit_is_clear(PIN, NEUTRAL_BIT);
+    bit = bit_is_clear(PIN, NEUTRAL_BIT);
 #endif
-        if (bit) gear = 0; // We're in neutral!
+    if (bit) gear = 0; // We're in neutral!
 
-        eeprom_write_byte((uint8_t*)0, gear);
-        showDigit(gear);
-        _delay_ms(LOCK_TIME);
-    }
+    eeprom_write_byte((uint8_t*)0, gear);
+    showDigit(gear);
+    delay(LOCK_TIME);
+  }
 
 
-    /***  DOWN SHIFT  ***/
-    if (debouce(DOWN_BIT))
-    {
-        gear > 1 ? gear-- : \
-        (gear == 0) ? gear=1 : NULL;
-        // if (gear > 1) gear--;
-        // if (gear == 0) gear = 1;
+  /***  DOWN SHIFT  ***/
+  if (debouce(DOWN_BIT))
+  {
+    //    gear > 1 ? gear-- : \
+    //    (gear == 0) ? gear = 1 : NULL;
+    if (gear > 1) gear--;
+    if (gear == 0) gear = 1;
 
-        uint8_t bit;
-#if (NEUTRAL_ACTIVE_LOW)
-        bit = !bit_is_clear(PIN, NEUTRAL_BIT);
+    uint8_t bit;
+#if (NEUTRAL_ACTIVE_HIGH)
+    bit = !bit_is_clear(PIN, NEUTRAL_BIT);
 #else
-        bit = bit_is_clear(PIN, NEUTRAL_BIT);
+    bit = bit_is_clear(PIN, NEUTRAL_BIT);
 #endif
-        if (bit) gear = 0; // We're in neutral!
+    if (bit) gear = 0; // We're in neutral!
 
-        eeprom_write_byte((uint8_t*)0, gear);
-        showDigit(gear);
-        _delay_ms(LOCK_TIME);
-    }
+    eeprom_write_byte((uint8_t*)0, gear);
+    showDigit(gear);
+    delay(LOCK_TIME);
+  }
 
 }
 
