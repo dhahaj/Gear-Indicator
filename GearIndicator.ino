298a4a24e8c9e9685fc72aa64309c97e51ccdacd
dhahaj <dhahaj@gmail.com>
2018-02-19 13:19:40 -0600

    slight modifications

--- GearIndicator.ino
+++ GearIndicator.ino
@@ -1,51 +1,41 @@
 //------------------------------------------
-// Motorcycle Gear Indicator Source v1.3
+// Motorcycle Gear Indicator Source v1.4
 // Created      : 8/4/2010
-// Last Updated : 2/16/2018
+// Last Updated : 2/19/2018
 //------------------------------------------
 
-#include <avr/io.h>
-#include <avr/eeprom.h>
 #include <EEPROM.h>
 
-// Uncomment to enable active high neutral input (+12V signal)
-//  else neutral will be active when the pin is pulled to ground.
+// Uncomment to enable an active high neutral input signal. Else,
+// the default assumption is that the neutral signal switches to ground
+//#define NEUTRAL_ACTIVE_HIGH 
+//#define SERIAL_DEBUG(x) (Serial.println((x)))
 
-#define NEUTRAL_ACTIVE_HIGH 0
-#define NEUTRAL_ACTIVE_LOW 1
-
-// PIN/PORT DEFINITIONS
-#define PORT          PORTB
-#define PIN           PINB
-#define DDPORT        DDRB
-#define RESET_BIT     0 //PB0
-#define CLOCK_BIT     1 //PB1
-#define NEUTRAL_BIT   2 //PB2
-#define UP_BIT        3 // PB3
-#define DOWN_BIT      4 //PB4
-#define RESET_DDR     DDB0
-#define CLOCK_DDR     DDB1
-#define EE_ADDR       0 //((uint8_t*)0)
+#define EE_ADDR 0
 
 // Timing Configurations
-#define DEBOUNCE_TIME 50
+#define DEBOUNCE_TIME 60
 #define LOCK_TIME     175
-
-// Specific Configurations
-#define TOP_GEAR      9
-#define SHOW_DIGITS(n) showDigit((n)); delay(575)
+#define TOP_GEAR      0x0F
 
 volatile uint8_t gear;
-int reset = 0, clock = 1, up = 3, down = 4, neutral = 5;
+uint8_t reset = 0,
+        clock = 1,
+        up = 3,
+        down = 4,
+        neutral = 2;
+//uint8_t reset = 13,
+//        clock = 12,
+//        up = 2,
+//        down = 3,
+//        neutral = 4;
 
 /*
    Display a digit on the 7-segment display.
 */
 void showDigit(uint8_t digit) {
   digitalWrite(reset, HIGH);
-  delay(1);
   digitalWrite(reset, LOW);
-
   for (uint8_t i = 0; i < digit; i++)
     digitalWrite(clock, !digitalRead(clock));
 }
@@ -61,28 +51,30 @@ bool debouce(uint8_t pin) {
   return false;
 }
 
-/*
-   Setup the function
-*/
 void setup(void) {
 
-  // Enable pullups
-  pinMode(up, INPUT_PULLUP);
-  pinMode(down, INPUT_PULLUP);
-#ifndef NEUTRAL_ACTIVE_HIGH // Include the neutral input pin if it is active low
-  pinMode(neutral, INPUT_PULLUP);
-#else
-  pinMode(neutral, INPUT);
+#ifdef SERIAL_DEBUG
+  Serial.begin(9600);
+  SERIAL_DEBUG("Gear Indicator Sketch begining..");
 #endif
 
+  pinMode(up, INPUT_PULLUP);
+  pinMode(down, INPUT_PULLUP);
+
+  // Do not use a pullup if we're going to use an active high input
+#ifdef NEUTRAL_ACTIVE_HIGH
+  pinMode(neutral, INPUT);
+#else
+  pinMode(neutral, INPUT_PULLUP);
+#endif
   pinMode(reset, OUTPUT);
   pinMode(clock, OUTPUT);
   digitalWrite(reset, LOW);
 
   selfTest();
 
-  gear = eeprom_read_byte(EE_ADDR); // Read initial value from EEPROM
-  showDigit(gear);  // Show initial value
+  gear = readGear(); // Read initial value from EEPROM
+  showDigit(gear);
 }
 
 /*
@@ -102,54 +94,81 @@ void loop(void) {
           gear++;
         break;
     }
-
     if (inNeutral()) gear = 0;
     showDigit(gear);
     saveGear(gear);
   }
 
   /***  DOWN SHIFT  ***/
-  if (debouce(DOWN_BIT))
+  if (debouce(down))
   {
     switch (gear) {
       case 0:
         gear = 1;
         break;
-      default:
+      case 1:
+        break;
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
         if (gear < TOP_GEAR)
           gear--;
         break;
+      default:
+        return;
     }
     if (inNeutral()) gear = 0;
     showDigit(gear);
     saveGear(gear);
   }
+
 }
 
 /*
    Saves the current gear to the eeprom.
 */
 void saveGear(uint8_t g) {
+#ifdef SERIAL_DEBUG
+  SERIAL_DEBUG("saving gear: ");
+  SERIAL_DEBUG(g);
+#endif
   EEPROM.update(EE_ADDR, g);
   delay(LOCK_TIME);
+}
+
+/*
+   Reads the stored gear from the eeprom. Also handles
+   cases where the value is unset.
+*/
+uint8_t readGear(void) {
+  uint8_t val;
+  EEPROM.get(EE_ADDR, val);
+#ifdef SERIAL_DEBUG
+  SERIAL_DEBUG("reading gear: ");
+  SERIAL_DEBUG(val);
+#endif
+  if (val > TOP_GEAR) {
+    delay(1);
+    saveGear(0);
+    return 0;
+  }
+  return val;
 }
 
 /*
    Returns true if the neutral signal is active.
 */
 boolean inNeutral(void) {
-#if (NEUTRAL_ACTIVE_HIGH & NEUTRAL_ACTIVE_LOW)
-#error "Conflicting values for active high/low neutral input!"
-#elif not(NEUTRAL_ACTIVE_HIGH | NEUTRAL_ACTIVE_LOW)
-#error "No definitions for active high or low neutral input!"
-#endif
-
-#if (NEUTRAL_ACTIVE_HIGH)
+#ifdef NEUTRAL_ACTIVE_HIGH
   return (digitalRead(neutral) == LOW);
-#elif (NEUTRAL_ACTIVE_LOW)
+#else
   return (digitalRead(neutral) == HIGH);
 #endif
-
 }
 
 /*
@@ -158,7 +177,13 @@ boolean inNeutral(void) {
 void selfTest(void) {
   showDigit(0);
   delay(350);
-  for (byte i = 1; i < 10; i++) {
+#ifdef SERIAL_DEBUG
+  SERIAL_DEBUG("Self Testing");
+#endif
+  for (int i = 1; i < 10; i++) {
+#ifdef SERIAL_DEBUG
+    SERIAL_DEBUG(i);
+#endif
     showDigit(i);
     delay(500);
   }
