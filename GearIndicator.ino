87dc504afee9a71ab812f0599b72904a9e55347a
dhahaj <dhahaj@gmail.com>
2018-02-19 11:51:37 -0600

    Modifying code to be more arduino friendly

--- GearIndicator.ino
+++ GearIndicator.ino
@@ -5,23 +5,27 @@
 //------------------------------------------
 
 #include <avr/io.h>
-//#include <util/delay.h>
 #include <avr/eeprom.h>
+#include <EEPROM.h>
 
-// Set to 1 to enable active low neutral input
-//#define NEUTRAL_ACTIVE_HIGH 0
+// Uncomment to enable active high neutral input (+12V signal)
+//  else neutral will be active when the pin is pulled to ground.
+
+#define NEUTRAL_ACTIVE_HIGH 0
+#define NEUTRAL_ACTIVE_LOW 1
 
 // PIN/PORT DEFINITIONS
 #define PORT          PORTB
 #define PIN           PINB
 #define DDPORT        DDRB
-#define RESET_BIT     PB0
-#define CLOCK_BIT     PB1
-#define NEUTRAL_BIT   PB2
-#define UP_BIT        PB3
-#define DOWN_BIT      PB4
+#define RESET_BIT     0 //PB0
+#define CLOCK_BIT     1 //PB1
+#define NEUTRAL_BIT   2 //PB2
+#define UP_BIT        3 // PB3
+#define DOWN_BIT      4 //PB4
 #define RESET_DDR     DDB0
 #define CLOCK_DDR     DDB1
+#define EE_ADDR       0 //((uint8_t*)0)
 
 // Timing Configurations
 #define DEBOUNCE_TIME 50
@@ -31,62 +35,54 @@
 #define TOP_GEAR      9
 #define SHOW_DIGITS(n) showDigit((n)); delay(575)
 
+volatile uint8_t gear;
+int reset = 0, clock = 1, up = 3, down = 4, neutral = 5;
+
 /*
    Display a digit on the 7-segment display.
 */
-void showDigit(uint8_t Digit) {
-  uint8_t i;
+void showDigit(uint8_t digit) {
+  digitalWrite(reset, HIGH);
+  delay(1);
+  digitalWrite(reset, LOW);
 
-  PORT |= (1 << RESET_BIT);       // RESET pin high
-  PORT &= ~(1 << RESET_BIT);      // RESET pin low
-
-  for (uint8_t i = 0; i < Digit; i++) {
-    PORT |= (1 << CLOCK_BIT);   // CLOCK pin high
-    PORT &= ~(1 << CLOCK_BIT);  // CLOCK pin low
-  }
+  for (uint8_t i = 0; i < digit; i++)
+    digitalWrite(clock, !digitalRead(clock));
 }
 
 /*
    Simple debouncing for the gear inputs
 */
-int debouce(uint8_t pin) {
-  if (bit_is_clear(PIN, pin)) {
+bool debouce(uint8_t pin) {
+  if (digitalRead(pin) == LOW) {
     delay(DEBOUNCE_TIME);
-    if (bit_is_set(PIN, pin)) return true;
+    if (digitalRead(pin) == HIGH) return true;
   }
   return false;
 }
-
-volatile uint8_t gear;
 
 /*
    Setup the function
 */
 void setup(void) {
 
-  //#if (NEUTRAL_ACTIVE_HIGH)
-  PORT = ( _BV(UP_BIT) | _BV(DOWN_BIT) | _BV(NEUTRAL_BIT) ); // Enable all pullups
-  //#else
-  //  PORT = ( _BV(UP_BIT) | _BV(DOWN_BIT) ); // UP and DOWN pin enable pull-up
-  //#endif
+  // Enable pullups
+  pinMode(up, INPUT_PULLUP);
+  pinMode(down, INPUT_PULLUP);
+#ifndef NEUTRAL_ACTIVE_HIGH // Include the neutral input pin if it is active low
+  pinMode(neutral, INPUT_PULLUP);
+#else
+  pinMode(neutral, INPUT);
+#endif
 
-  DDPORT |= _BV(RESET_DDR);      // RESET pin as output
-  DDPORT |= _BV(CLOCK_DDR);      // CLOCK pin as output
+  pinMode(reset, OUTPUT);
+  pinMode(clock, OUTPUT);
+  digitalWrite(reset, LOW);
 
-  PORT &= ~(_BV(RESET_BIT));  // RESET pin low
+  selfTest();
 
-  // Self test effect
-  showDigit(0);
-  delay(350);
-  for (uint8_t i = 1; i < 10; i++) {
-    showDigit(i);
-    delay(500);
-  }
-  showDigit(0);
-  delay(500);
-
-  gear = eeprom_read_byte((uint8_t*)0);  // Read initial value from EEPROM
-  showDigit(gear);                       // Show initial value
+  gear = eeprom_read_byte(EE_ADDR); // Read initial value from EEPROM
+  showDigit(gear);  // Show initial value
 }
 
 /*
@@ -95,49 +91,79 @@ void setup(void) {
 void loop(void) {
 
   /***  UP SHIFT  ***/
-  if (debouce(UP_BIT))
+  if (debouce(up))
   {
-    // Some logic here :-)
-    //        (gear > 0 && gear < TOP_GEAR) ? gear++ : \
-    //        (gear == 0) ? gear = 2 : NULL;
-    if (gear == 0) gear = 2;
-    else if ( (gear > 0) && (gear < TOP_GEAR) ) gear++;
+    switch (gear) {
+      case 0:
+        gear = 2;
+        break;
+      default:
+        if (gear < TOP_GEAR)
+          gear++;
+        break;
+    }
 
-    uint8_t bit;
-#if (NEUTRAL_ACTIVE_HIGH)
-    bit = !bit_is_clear(PIN, NEUTRAL_BIT);
-#else
-    bit = bit_is_clear(PIN, NEUTRAL_BIT);
-#endif
-    if (bit) gear = 0; // We're in neutral!
-
-    eeprom_write_byte((uint8_t*)0, gear);
+    if (inNeutral()) gear = 0;
     showDigit(gear);
-    delay(LOCK_TIME);
+    saveGear(gear);
   }
-
 
   /***  DOWN SHIFT  ***/
   if (debouce(DOWN_BIT))
   {
-    //    gear > 1 ? gear-- : \
-    //    (gear == 0) ? gear = 1 : NULL;
-    if (gear > 1) gear--;
-    if (gear == 0) gear = 1;
-
-    uint8_t bit;
-#if (NEUTRAL_ACTIVE_HIGH)
-    bit = !bit_is_clear(PIN, NEUTRAL_BIT);
-#else
-    bit = bit_is_clear(PIN, NEUTRAL_BIT);
-#endif
-    if (bit) gear = 0; // We're in neutral!
-
-    eeprom_write_byte((uint8_t*)0, gear);
+    switch (gear) {
+      case 0:
+        gear = 1;
+        break;
+      default:
+        if (gear < TOP_GEAR)
+          gear--;
+        break;
+    }
+    if (inNeutral()) gear = 0;
     showDigit(gear);
-    delay(LOCK_TIME);
+    saveGear(gear);
   }
+}
 
+/*
+   Saves the current gear to the eeprom.
+*/
+void saveGear(uint8_t g) {
+  EEPROM.update(EE_ADDR, g);
+  delay(LOCK_TIME);
+}
+
+/*
+   Returns true if the neutral signal is active.
+*/
+boolean inNeutral(void) {
+#if (NEUTRAL_ACTIVE_HIGH & NEUTRAL_ACTIVE_LOW)
+#error "Conflicting values for active high/low neutral input!"
+#elif not(NEUTRAL_ACTIVE_HIGH | NEUTRAL_ACTIVE_LOW)
+#error "No definitions for active high or low neutral input!"
+#endif
+
+#if (NEUTRAL_ACTIVE_HIGH)
+  return (digitalRead(neutral) == LOW);
+#elif (NEUTRAL_ACTIVE_LOW)
+  return (digitalRead(neutral) == HIGH);
+#endif
+
+}
+
+/*
+   Run a display self test.
+*/
+void selfTest(void) {
+  showDigit(0);
+  delay(350);
+  for (byte i = 1; i < 10; i++) {
+    showDigit(i);
+    delay(500);
+  }
+  showDigit(0);
+  delay(500);
 }
 
 
